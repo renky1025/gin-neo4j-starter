b47ea177946b392320d1aa2f97ac231662232230
shuyu-renky <kangyao.ren@digitsvalue.com>
2023-11-14 16:10:31 +0800

    update cros func

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +8,12 @@ import (
 // Cors CORS（跨域资源共享）中间件
 func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{"*"},
-		AllowMethods: []string{"OPTIONS", "GET", "POST", "POST", "DELETE", "PUT"},
-		MaxAge:       1 * time.Hour,
+		AllowMethods:     []string{"GET", "PATCH", "POST", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "lang", "Authorization", "accept", "origin", "Content-Type", "User-Agent", "Referrer", "Host", "Token", "X-Requested-With", "Cache-Control", "x-tenant", "x-client", "X-CSRF-Token", "Content-Type", "access-control-allow-origin", "access-control-allow-headers", "Content-Length", "Accept-Encoding"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+		AllowOriginFunc:  func(origin string) bool { return true },
+		MaxAge:           86400,
 	})
 }
